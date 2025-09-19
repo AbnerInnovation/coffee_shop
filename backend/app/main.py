aa66242e51ad8a3a5688e4e823a2428c43e304a3
Abner Soto <abners@zoomcatalog.com>
2025-09-18 22:21:59 -0700

    Add cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -99,7 +99,7 @@ app.openapi = custom_openapi
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Frontend URL
+    allow_origins=["http://localhost:3000",'https://www.shopacoffee.com'],  # Frontend URL
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allow_headers=["*"],
