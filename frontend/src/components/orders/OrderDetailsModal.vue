a607f45921e4f36e62ffebcfd247f3705a3b26cf
Abner Soto <abners@zoomcatalog.com>
2025-10-09 18:47:42 -0700

    ...

--- frontend/src/components/orders/OrderDetailsModal.vue
+++ frontend/src/components/orders/OrderDetailsModal.vue
@@ -169,7 +169,7 @@
                     v-else-if="order.status === 'ready'"
                     type="button"
                     class="inline-flex w-full justify-center rounded-md border border-transparent bg-green-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 sm:col-start-2 sm:text-sm"
-                    @click="updateStatus('Completed')"
+                    @click="updateStatus('completed')"
                   >
                     {{$t('app.views.orders.modals.details.mark_completed')}}
                   </button>
